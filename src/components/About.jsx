cfbfc8b4893205254423c56f1bdeec74e16deadc
AbhinavKumar777 <abhinavdornala777@gmail.com>
2023-06-04 22:10:39 +0530

    Uploaded files

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Hello there! My name is Abhinav, and I am a web developer. With a passion for creating dynamic and user-friendly websites, I have honed my skills in various programming languages and frameworks to bring ideas to life on the digital canvas. Equipped with a strong foundation in HTML, CSS, JavaScript, and a knack for problem-solving, I strive to deliver engaging online experiences that captivate users and exceed their expectations. Whether it's building responsive layouts, implementing interactive features, or optimizing performance, I am committed to crafting seamless and visually appealing websites that leave a lasting impression. Through continuous learning and staying up-to-date with the latest industry trends, I am dedicated to refining my craft and staying at the forefront of web development. It's my pleasure to be a part of this ever-evolving field and contribute to the advancement of the digital landscape.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
